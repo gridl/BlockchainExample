324d3116c9f932c6abc9f5139cd3de2e855c0148
Vishwanath Subramanian <vsubrama@vsubrama013074.starbucks.net>
2018-01-07 10:29:29 -0800

    proof of work

--- POW.py
+++ POW.py
@@ -1,10 +1,40 @@
-from hashlib import sha256
+import hashlib
+import json
 
-x = 5
-y = 0 # TBD
+from time import time
+from uuid import uuid4
 
-while sha256(f'{x*y}'.encode()).hexdigest()[-1] != '0':
-    y += 1
 
-    print(f'The solution is y = {y}')
+class Blockchain(object):
+
+    def proof_of_workf(self, last_proof):
+        """
+        Simple proof of work algorithm:
+        - Find a number 'p' such as hash(pp') contains leading 4 zeroes, where p is the previous p'
+        - p is the previous proof, and p' is the new proof
+
+        :param last_proof:
+        :return:
+        """
+
+        proof = 0
+        while self.valid_proof(last_proof,proof) is False:
+            print(last_proof)
+            print(proof)
+            proof += 1
+
+        return proof
+
+    @staticmethod
+    def valid_proof(last_proof, proof):
+        """
+        Validates the proof: Does hash(last_proof, proof) contain 4 leading zeros?
+        :param last_proof:
+        :param proof:
+        :return: <bool> True if connet, False f not.
+        """
+        guess = f'{last_proof}{proof}'.encode()
+        guess_hash = hashlib.sha256(guess).hexdigest()
+        return guess_hash[:4] == "0000"
+
 
